dfb3d64a6f2528336906795dafadb25bb893bf7a
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-06-14 08:30:50 +0300

    First commit

--- lib/Stack.py
+++ lib/Stack.py
@@ -8,22 +8,36 @@ class Stack:
         return len(self.items)==0
 
     def push(self, item):
-        if len(self.items)<self.limt:
+        if len(self.items) < self.limit:
             self.items.append(item)
         else:
             print("stack is full. Cannot push item. ")
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            print("stack is empty. Cannot pop item. ")
+            return None
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            print("stack is empty. Cannot peek item.")
+            return None
+    
     
     def size(self):
-        pass
+        return len(self.items)
+    
 
     def full(self):
-        pass
+        return len(self.items)==self.limit
+    
 
     def search(self, target):
-        pass
+        for i in reversed(range(len(self.items))):
+            if self.items[i] == target:
+                return len(self.items) -1 
+            return -1 
